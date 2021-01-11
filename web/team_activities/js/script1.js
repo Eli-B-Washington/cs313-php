7af797df9aa776595baf192bb01b81cb6308b679
eli-b-washington <bea14018@byui.edu>
2021-01-11 12:58:07 -0800

    commit changes for team activity 1

---
+++ web/team_activities/js/script1.js
@@ -0,0 +1,8 @@
+function buttonClick(){
+    alert("Clicked!");
+}
+
+function changeColor(){
+    let x = document.getElementById("input1").value;
+    document.getElementById("div1").style.backgroundColor = x;
+}
