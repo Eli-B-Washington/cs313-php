b6aed64068fd00fed19ec1a85582ee3d1daa1b5c
eli-b-washington <bea14018@byui.edu>
2021-02-01 10:45:09 -0800

    commit on master

--- web/team_activities/team_activity_4.php
+++ web/team_activities/team_activity_4.php
@@ -27,24 +27,6 @@ echo "Today is " . date("m/d/Y") . "<br>";
 <?php
 try
 {
-  $user = 'postgres';
-  $password = 'password';
-  $db = new PDO('pgsql:host=localhost;dbname=myTestDB', $user, $password);
-
-  // this line makes PDO give us an exception when there are problems,
-  // and can be very helpful in debugging! (But you would likely want
-  // to disable it for production environments.)
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
-
-
-try
-{
   $dbUrl = getenv('DATABASE_URL');
 
   $dbOpts = parse_url($dbUrl);
