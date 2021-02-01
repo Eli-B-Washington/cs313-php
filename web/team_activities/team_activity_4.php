34ccb4e9e75c3fe1afd8fea2d1c14583d712d496
eli-b-washington <bea14018@byui.edu>
2021-02-01 10:30:48 -0800

    commit new chages to team assignment 4

--- web/team_activities/team_activity_4.php
+++ web/team_activities/team_activity_4.php
@@ -41,6 +41,39 @@ catch (PDOException $ex)
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
+
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+{
+  echo 'user: ' . $row['book'];
+  echo ' chapter: ' . $row['chapter'];
+  echo ' verse: ' . $row['verse'];
+  echo ' content: ' . $row['content'];
+  echo '<br/>';
+}
 ?>
 
     </ul>
