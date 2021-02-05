959ca334a8bf6fdcb6f46687dc3ff04ecdfa625b
eli-b-washington <bea14018@byui.edu>
2021-02-05 10:59:49 -0800

    changes to index

--- web/assignment5/index.php
+++ web/assignment5/index.php
@@ -47,8 +47,9 @@ catch (PDOException $ex)
 }
 
 
-foreach ($db->query('SELECT title FROM public.boardGame') as $row)
+foreach ($db->query('SELECT title, author FROM public.boardGame') as $row)
 {  echo 'Title: ' . $row['title'];
+    echo 'Publisher: ' . $row['author'];
 echo '<br/>';
 }
 echo  $db->prepare('SELECT * FROM public.boardGame ');
