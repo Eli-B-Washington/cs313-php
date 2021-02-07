949690a4e7f513d57987cf2604c19dc5e40cca13
eli-b-washington <bea14018@byui.edu>
2021-02-06 19:12:15 -0800

    added changes to search

--- web/assignment5/search.php
+++ web/assignment5/search.php
@@ -1,9 +1,55 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+$output = '';
+if(isset($POST['search'])){
+  $searchq = $_POST['search'];
+  $searchq = preg_replace('#[^0-9a-z]#i', "", $searchq);
+
+  $query = mysql_query("SELECT * FROM public.boardGame WHERE title LIKE '%$searchq$%' ") or die("could not search database");
+  $count = mysql_num_rows($query);
+  if ($count == 0){
+   $output = 'There was no search results!';
+  }
+  else{
+
+  }
+  while($row = mysql_fetch_array($query)){
+    $title = $row['title'];
+
+    $output = $title;
+  }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Introduction Page</title>
+    <title>Search Page</title>
     <meta name="description" content="A page that displays hello world">
     <link rel="stylesheet" href="css/style.css">
 </head>
@@ -22,10 +68,14 @@
 </nav>
 </header>
 <main>
-   <div>
-  <p>
-  </p>
-  </div>
+  <form action="search.php" method="post">
+  <input type="text" name= "search" placeholder="Search for Members">
+  <input type="submit" value=">>">
+  </form>
+
+  <?php
+  print('$output');
+  ?>
 </main>
 </body>
 
