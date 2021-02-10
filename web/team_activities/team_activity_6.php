cdb9203edbef4c9e731c3b2dd6b717e952a126bc
eli-b-washington <bea14018@byui.edu>
2021-02-10 12:00:32 -0800

    commit on master

---
+++ web/team_activities/team_activity_6.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction Page</title>
+    <meta name="description" content="A page that displays hello world">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+
+<body>
+<header>
+  <h1>Introduction</h1>
+  <nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="assignments.php">Assignment Portal</a></li>
+        <li><?php
+echo "Today is " . date("m/d/Y") . "<br>";
+?></li>
+    </ul>
+</nav>
+</header>
+<main>
+    <h2>Assignment Links <br>
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+
+foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+{
+  echo 'user: ' . $row['book'];
+  echo ' chapter: ' . $row['chapter'];
+  echo ' verse: ' . $row['verse'];
+  echo ' content: ' . $row['content'];
+  echo '<br/>';
+}
+echo  $db->prepare('SELECT * FROM scriptures ');
+
+
+
+
+
+
+
+?>
+
+<form action="">
+<input type="text">
+<input type="text">
+<input type="text">
+<input type="text">
+<input type="submit">
+</form>
+
+    </ul>
+</main>
+</body>
+
+</html>
