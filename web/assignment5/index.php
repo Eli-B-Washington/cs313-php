4252b960933e0cacb8339b2e4a27ec1509696460
eli-b-washington <bea14018@byui.edu>
2021-02-17 08:14:03 -0800

    commit on master

--- web/assignment5/index.php
+++ web/assignment5/index.php
@@ -40,7 +40,7 @@ echo "<table>";
 foreach ($db->query('SELECT title, author, rating, type, playersmin, playersmax, cooperative, length FROM public.boardGame') as $row)
 {  
   echo "<tr><td>" . $row['title'] . "</td><td>" . $row['author'] ."</td><td>"
-  . $row['rating'] . "</td></td>" . $row['type'] . "</td><td>"
+  . $row['rating'] . "</td><td>" . $row['type'] . "</td><td>"
   . $row['playersmin'] . "</td><td>". $row['playersmax'] . "</td><td>"
   . $row['cooperative'] . "</td><td>" . $row['length'] . "</td></tr>";
 
