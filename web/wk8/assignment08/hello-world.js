88a8658e574c391e97953e8e114ffe8bbd6f05eb
eli-b-washington <bea14018@byui.edu>
2021-02-26 15:47:31 -0800

    commit on master

---
+++ web/wk8/assignment08/hello-world.js
@@ -0,0 +1,38 @@
+// Load the http module to create an http server.
+const http = require('http');
+const hostname = 'localhost';
+const port = 8888;
+
+//configure http server to respond with the approptiate response
+var server = http.createServer(onRequest);
+
+
+function onRequest(request, response) {
+
+if (request.url == '/home'){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Welcome to the Home Page\n");
+  response.end();
+    }
+ else if (request.url == '/getData'){
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        const fs = require('fs');
+        let rawdata = fs.readFileSync('student.json');
+        let student = JSON.parse(rawdata);
+        response.write(student.name + " " + student.class);
+        response.end();
+          }
+else{
+  response.writeHead(404, {"Content-Type": "text/html"});
+  response.write("Incorrect Address\n");
+        response.end();
+}
+}
+
+
+
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
